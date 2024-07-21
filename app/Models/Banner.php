f5743b667fcd955fe4ec821b9f11ec35f489e535
phuc1903 <dinhtrongphuc19@gmail.com>
2024-07-21 23:32:05 +0700

    update migrate banners and update Model Banner

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Banner extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'collection',
+        'title',
+        'description',
+        'link',
+        'image',
+        'banner_show',
+        'action',
+        'backgound'
+    ];
 }
